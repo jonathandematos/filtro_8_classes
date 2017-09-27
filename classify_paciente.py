9b03fdf0a88a42302fb9db9ea4fb812efa5c46a0
Jonathan de Matos <jonathandematos@gmail.com>
2017-09-26 21:40:30 -0300

    classify_paciente.py com combinacao implementada

--- classify_paciente.py
+++ classify_paciente.py
@@ -7,101 +7,189 @@ import numpy as np
 import joblib
 import os
 #
+# Combine results by vote
+#
+def CombineByVote(results):
+    if(len(results)>0):
+        if(len(results[0])>0):
+            vote_list = [0 for i in range(len(results[0]))]
+            for i in results:
+                vote_list[np.argmax(np.array(i))] += 1
+            return np.argmax(np.array(vote_list))
+        return -1
+    return -1
+#
+# Combine results by sum
+#
+def CombineBySum(results):
+    if(len(results)>0):
+        if(len(results[0])>0):
+            vote_list = [0 for i in range(len(results[0]))]
+            for i in results:
+                for j in range(len(i)):
+                    vote_list[j] += i[j]
+            return np.argmax(np.array(vote_list))
+        return -1
+    return -1
+#
 #f = open("pftas_filtro_150.txt","r")
-f = open("svm_tissues/pftas_file_150.txt","r")
+##f = open("svm_tissues/pftas_file_150.txt","r")
+##
+#X = list()
+#Y = list()
+#Z = list()
+#W = list()
+#U = list()
+#for i in f:
+#    linha = i[:-1].split(";")
+#    x_tmp = list()
+#    for j in linha[2:-1]:
+#        x_tmp.append(float(j))
+#    if(len(x_tmp) != 162):
+#        continue
+#    X.append(x_tmp)
+#    class_str = linha[0]
+#    #
+#    # 0 - importante
+#    # 1 - irrelevante
+#    #
+#    if(class_str == 'adenosis'):
+#    	class_line = int(0)
+#    if(class_str == 'ductal_carcinoma'):
+#    	class_line = int(4)
+#    if(class_str == 'fibroadenoma'):
+#    	class_line = int(1)
+#    if(class_str == 'lobular_carcinoma'):
+#    	class_line = int(5)
+#    if(class_str == 'mucinous_carcinoma'):
+#    	class_line = int(6)
+#    if(class_str == 'papillary_carcinoma'):
+#    	class_line = int(7)
+#    if(class_str == 'phyllodes_tumor'):
+#    	class_line = int(2)
+#    if(class_str == 'tubular_adenoma'):
+#	class_line = int(3)
+#    #
+#    Y.append(class_line)
+#    Z.append(linha[1])
+#    a = linha[1].split("-")
+#    W.append(str(a[0])+"-"+str(a[1])+"-"+str(a[2])+"-"+str(a[3])+"-"+str(a[4]))
+#    U.append(linha[1])
+##
+#f.close()
+##
+##X_train, X_test, Y_train, Y_test, Z_train, Z_test = train_test_split(X, Y, Z, test_size=0.3)
+##
+#Z_test = list()
+#Z_train = list()
+##
+#f = open("svm_tissues/dsfold1.txt","r")
+#for i in f:
+#    linha = i[:-1].split("|")
+#    img = linha[0].split(".")[0]
+#    if(linha[3] == "train"):
+#        Z_train.append(img)
+#    else:
+#        Z_test.append(img)
+#f.close()
+##
+#X_test = list()
+#Y_test = list()
+#U_test = list()
+#X_train = list()
+#Y_train = list()
+#U_train = list()
+#for i in range(len(X)):
+#    if(W[i] in Z_test):
+#        X_test.append(X[i])
+#        Y_test.append(Y[i])
+#        U_test.append(U[i])
+#    if(W[i] in Z_train):
+#        X_train.append(X[i])
+#        Y_train.append(Y[i])
+#        U_train.append(U[i])
+##
+#del X
+#del Y
+#del U
+#del Z_test
+#del Z_train
+##
+#print(len(X_test), len(X_train))
+##
+##
+##
+#tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1, 8e-1, 7e-1, 6e-1, 4e-1, 2e-1, 1e-1],
+#                     'C': [5e-1, 5, 50, 500, 5000, 60000]},
+#                    {'kernel': ['linear'], 'C': [1e-1, 1, 10, 100, 1000, 5000]}]
+##
+##clf = GridSearchCV(SVC(probability=True), tuned_parameters, cv=5, scoring='accuracy', n_jobs=4)
+#clf = SVC(probability=True)
+#clf.fit(X_train, Y_train)
+##
+##print(clf.score(X_test, Y_test))
+##
+#pacs = {}
+#imgs = {}
+##
+## SOB_B_A-14-22549G-100-030-550-150.png
+##
+#for i in range(len(X_test)):
+#    str_img = U_test[i].split("-")
+#    img = str(str_img[2])+str(str_img[3])+str(str_img[4])
+#    pac = str(str_img[2])
+#    pred = np.squeeze(clf.predict_proba(np.array([X_test[i]])))
+#    #
+#    if(img in imgs):
+#        imgs[img][1].append(pred)
+#    else:
+#        imgs[img] = [Y_test[i],list()]
+#        imgs[img][1].append(pred)
+#    #
+#    if(pac in pacs):
+#        pacs[pac][1].append(pred)
+#    else:
+#        pacs[pac] = [Y_test[i],list()]
+#        pacs[pac][1].append(pred)
+##
+#joblib.dump(pacs, "pacs.pkl")
+#joblib.dump(imgs, "imgs.pkl")
+#exit(0)
 #
-X = list()
-Y = list()
-Z = list()
-W = list()
-U = list()
-for i in f:
-    linha = i[:-1].split(";")
-    x_tmp = list()
-    for j in linha[2:-1]:
-        x_tmp.append(float(j))
-    if(len(x_tmp) != 162):
-        continue
-    X.append(x_tmp)
-    class_str = linha[0]
-    #
-    # 0 - importante
-    # 1 - irrelevante
-    #
-    if(class_str == 'adenosis'):
-    	class_line = int(0)
-    if(class_str == 'ductal_carcinoma'):
-    	class_line = int(4)
-    if(class_str == 'fibroadenoma'):
-    	class_line = int(1)
-    if(class_str == 'lobular_carcinoma'):
-    	class_line = int(5)
-    if(class_str == 'mucinous_carcinoma'):
-    	class_line = int(6)
-    if(class_str == 'papillary_carcinoma'):
-    	class_line = int(7)
-    if(class_str == 'phyllodes_tumor'):
-    	class_line = int(2)
-    if(class_str == 'tubular_adenoma'):
-	class_line = int(3)
-    #
-    Y.append(class_line)
-    Z.append(linha[1])
-    a = linha[1].split("-")
-    W.append(str(a[0])+"-"+str(a[1])+"-"+str(a[2])+"-"+str(a[3])+"-"+str(a[4]))
-    U.append(linha[1])
+pacs = joblib.load("pacs.pkl")
+imgs = joblib.load("imgs.pkl")
+#print(pacs)
+#exit(0)
+correto = 0
+total = 0
+for i in imgs:
+    if(imgs[i][0] == CombineBySum(imgs[i][1])):
+        correto += 1
+    total += 1
+print("Combinacao de imagens por soma: {}".format(float(correto)/total))
 #
-f.close()
+correto = 0
+total = 0
+for i in imgs:
+    if(imgs[i][0] == CombineByVote(imgs[i][1])):
+        correto += 1
+    total += 1
+print("Combinacao de imagens por voto: {}".format(float(correto)/total))
 #
-#X_train, X_test, Y_train, Y_test, Z_train, Z_test = train_test_split(X, Y, Z, test_size=0.3)
+correto = 0
+total = 0
+for i in pacs:
+    if(pacs[i][0] == CombineBySum(pacs[i][1])):
+        correto += 1
+    total += 1
+print("Combinacao de paciente por soma: {}".format(float(correto)/total))
 #
-Z_test = list()
-Z_train = list()
-#
-f = open("svm_tissues/dsfold1.txt","r")
-for i in f:
-    linha = i[:-1].split("|")
-    img = linha[0].split(".")[0]
-    if(linha[3] == "train"):
-        Z_train.append(img)
-    else:
-        Z_test.append(img)
-f.close()
-#
-X_test = list()
-Y_test = list()
-U_test = list()
-X_train = list()
-Y_train = list()
-U_train = list()
-for i in range(len(X)):
-    if(W[i] in Z_test):
-        X_test.append(X[i])
-        Y_test.append(Y[i])
-        U_test.append(U[i])
-    if(W[i] in Z_train):
-        X_train.append(X[i])
-        Y_train.append(Y[i])
-        U_train.append(U[i])
-#
-del X
-del Y
-del U
-del Z_test
-del Z_train
-#
-print(len(X_test), len(X_train))
-#
-#
-#
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1, 8e-1, 7e-1, 6e-1, 4e-1, 2e-1, 1e-1],
-                     'C': [5e-1, 5, 50, 500, 5000, 60000]},
-                    {'kernel': ['linear'], 'C': [1e-1, 1, 10, 100, 1000, 5000]}]
-#
-#clf = GridSearchCV(SVC(probability=True), tuned_parameters, cv=5, scoring='accuracy', n_jobs=4)
-clf = SVC()
-clf.fit(X_train, Y_train)
-#
-print(clf.score(X_test, Y_test))
+correto = 0
+total = 0
+for i in pacs:
+    if(pacs[i][0] == CombineByVote(pacs[i][1])):
+        correto += 1
+    total += 1
+print("Combinacao de paciente por voto: {}".format(float(correto)/total))
 #
 exit(0)
